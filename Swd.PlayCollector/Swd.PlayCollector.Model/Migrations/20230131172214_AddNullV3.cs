379c3377a85b2074f90ef80d57e08f321a58ce8a
BobanMilojevic <milojevic@stea.at>
2023-01-31 19:03:23 +0100

    Create IGenericRepository.cs

---
+++ Swd.PlayCollector/Swd.PlayCollector.Model/Migrations/20230131172214_AddNullV3.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Swd.PlayCollector.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullV3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "Theme",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "Location",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "CollectionItem",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "Theme",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "Location",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "CollectionItem",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25,
+                oldNullable: true);
+        }
+    }
+}
---
+++ Swd.PlayCollector/Swd.PlayCollector.Repository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Swd.PlayCollector.Model.Migrations;
+
+namespace Swd.PlayCollector.Repository;
+
+public interface IGenericRepository<TEntity> where TEntity : class, new()
+{
+    DbSet<TEntity> DbSet { get; }
+
+    //Create
+    void Add(TEntity t);
+    Task AddAsync(TEntity t);
+    
+    // Read
+    IQueryable<TEntity> GetAll();
+    Task<IQueryable<TEntity>> GettAllAsync();
+    
+    // Update
+    void Update(TEntity t, object key);
+    Task UpdateAsync(TEntity t, object key);
+    
+    // Delete
+    void Delete(object key);
+    Task DeleteAsync(object key);
+}
