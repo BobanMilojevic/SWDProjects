293583500d39bb02ed1102cb4ffd19f4012b337a
BobanMilojevic <milojevic@stea.at>
2023-02-02 17:30:12 +0100

    Change to Wifi SSD

--- Swd.PlayCollector/Swd.PlayCollector.Gui.Wpf/obj/Debug/net6.0-windows/App.g.cs
+++ Swd.PlayCollector/Swd.PlayCollector.Gui.Wpf/obj/Debug/net6.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace Swd.PlayCollector.Gui.Wpf {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace Swd.PlayCollector.Gui.Wpf {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             Swd.PlayCollector.Gui.Wpf.App app = new Swd.PlayCollector.Gui.Wpf.App();
             app.InitializeComponent();
