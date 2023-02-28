9555ca7bebc4896d06473b1d8df94fee11bf6a07
BobanMilojevic <milojevic@stea.at>
2023-02-28 21:17:02 +0100

    Implement Community Toolkit Mvvm

--- Swd.Bsp.Binding/Swd.Bsp.Binding/View/fForeCast.xaml.cs
+++ Swd.Bsp.Binding/Swd.Bsp.Binding/View/fForeCast.xaml.cs
@@ -4,99 +4,38 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
 using Swd.Bsp.Binding.Model;
+using Swd.Bsp.Binding.ViewModel;
 
 namespace Swd.Bsp.Binding.View;
 
 public partial class fForeCast : Window
 {
-    private string resultString;
     private Window _callerWindow;
-    private int _minDaysForForeCast = 1;
-    private int _maxDaysForForeCast = 10;
     
-    public List<int> DaysForForecast { get; set; }
-    public bool IsDegree { get; set; }
-    
-    public Window CallerWindow 
-    {
-        get
-        {
-            return _callerWindow;
-        }
-        set
-        {
-            _callerWindow = value;
-        } 
-    }
     
     public fForeCast()
     {
         InitializeComponent();
-
-        this.IsDegree = true;
-        
-        DaysForForecast = new List<int>();
-        for (int i = _minDaysForForeCast; i <= _maxDaysForForeCast; i++)
-        {
-            DaysForForecast.Add(i);
-        }
-        
-        this.DataContext = this;
+        this.DataContext = new fForecastViewModel();
     }
-
+    
     public fForeCast(Window callerWindows) : this()
     {
         _callerWindow = callerWindows;
     }
-
-    private void BtnShowForecast_OnClick(object sender, RoutedEventArgs e)
-    {
-        List<ForeCast> foreCasts = new List<ForeCast>();
-
-        int daysFoForeCast = (int)this.cbxDays.SelectedItem;
-        Random random = new Random();
-        
-        for (int i = 0; i < daysFoForeCast; i++)
-        {
-            ForeCast foreCast = new ForeCast
-            {
-                GeneralForecast = (GeneralForecast)random.Next(Enum.GetValues(typeof(GeneralForecast)).Length),
-                TemperatureLow = 0,
-                TemperatureHigh = 20
-            };
-            foreCasts.Add(foreCast);
-        }
-
-        this.lstForeCasts.DataContext = foreCasts;
-    }
-
-    //public int[] ComboBoxData { get; set; }
-
-    public void GetForcast()
-    {
-        
-        
-    }
     
-    private void FForeCast_OnLoaded(object sender, RoutedEventArgs e)
+    
+    public List<int> DaysForForecast { get; set; }
+    
+    public Window CallerWindow 
     {
-        // Variante 1: Comboxitems zur Liste hinzufügen
-        // for (int i = _minDaysForForeCast; i <= _maxDaysForForeCast; i++)
-        // {
-        //     ComboBoxItem item = new ComboBoxItem();
-        //     item.Content = i;
-        //     this.cbxDays.Items.Add(item);
-        // }
-
-        // Variante 2: Mit Itemsource
-        // List<int> daysForForecast = new List<int>();
-        // for (int i = _minDaysForForeCast; i <= _maxDaysForForeCast; i++)
-        // {
-        //     daysForForecast.Add(i);
-        // }
-        // this.cbxDays.ItemsSource = daysForForecast;
+        get { return _callerWindow; }
+        set { _callerWindow = value; } 
     }
 
+    
+    
+    private string resultString;
     private void BtnLoadRealtimeData_OnClick(object sender, RoutedEventArgs e)
     {
         LoadData();
--- Swd.Bsp.Binding/Swd.Bsp.Binding/ViewModel/fForecastViewModel.cs
+++ Swd.Bsp.Binding/Swd.Bsp.Binding/ViewModel/fForecastViewModel.cs
@@ -1,6 +1,52 @@
-﻿namespace Swd.Bsp.Binding.ViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Swd.Bsp.Binding.Model;
+
+namespace Swd.Bsp.Binding.ViewModel;
 
 public class fForecastViewModel
 {
+    // Fields
+    private int _minDaysForForeCast = 1;
+    private int _maxDaysForForeCast = 10;
+
+    public fForecastViewModel()
+    {
+        this.IsDegree = true;
+        DaysForForecast = BuildDaysForForecastList(_minDaysForForeCast, _maxDaysForForeCast);
+    }
     
+    // Properties
+    public bool IsDegree { get; set; }
+    public List<int> DaysForForecast { get; set; }
+    public List<ForeCast> ForecastList { get; set; }
+
+    
+    private List<int> BuildDaysForForecastList(int _minDaysForForeCast, int _maxDaysForForeCast)
+    {
+        return Enumerable.Range(_minDaysForForeCast, _maxDaysForForeCast).ToList();
+    }
+
+    private void GetForcast()
+    {
+        List<ForeCast> foreCasts = new List<ForeCast>();
+
+        //int daysFoForeCast = (int)this.cbxDays.SelectedItem;
+        int daysFoForeCast = 5;
+        Random random = new Random();
+        
+        for (int i = 0; i < daysFoForeCast; i++)
+        {
+            ForeCast foreCast = new ForeCast
+            {
+                GeneralForecast = (GeneralForecast)random.Next(Enum.GetValues(typeof(GeneralForecast)).Length),
+                TemperatureLow = 0,
+                TemperatureHigh = 20
+            };
+            foreCasts.Add(foreCast);
+        }
+
+        ForecastList = foreCasts;
+    }
 }
